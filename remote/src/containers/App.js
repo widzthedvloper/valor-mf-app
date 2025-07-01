04d9fd2767b77975fa72b7b29cc4dedd43a70680
Widzmarc Phelle <widzmarc@gmail.com>
2025-07-01 17:18:26 -0400

    Clean readme

--- remote/src/containers/App.js
+++ remote/src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import './App.css'; 
 
 
 class App extends Component {
--- remote/src/index.js
+++ remote/src/index.js
@@ -1,11 +1,11 @@
 import React, {Suspense} from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
-import './index.css'; 
 // import App from './containers/App';
 import 'tachyons';
 // import registerServiceWorker from './registerServiceWorker';
 const App = React.lazy(() => import('./containers/App'));
+import '@shared/styles.css';
 
 
 const root = createRoot(document.getElementById('root'));
