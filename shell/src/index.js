daa2ea4b9e1bda7012f05ac8277f563677b57769
Widzmarc Phelle <widzmarc@gmail.com>
2025-07-01 14:48:02 -0400

    Initial commit

---
+++ shell/src/index.js
@@ -0,0 +1,5 @@
+// You can write your own logic here to determine the actual url
+window.remoteUrl = ""
+
+// Use dynamic import here to allow webpack to interface with module federation code
+import("./bootstrap");
