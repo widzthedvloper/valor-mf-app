daaa596ba34b2304d96062c35d92ebdce69063a0
Widzmarc Phelle <widzmarc@gmail.com>
2025-07-01 18:11:35 -0400

    Change url for prod

--- shell/src/index.js
+++ shell/src/index.js
@@ -1,5 +1,5 @@
 // You can write your own logic here to determine the actual url
-window.remoteUrl = "http://localhost:3002"
+window.remoteUrl = `${window.location.protocol}//${window.location.hostname}:3002/remoteEntry.js`;
 
 // Use dynamic import here to allow webpack to interface with module federation code
 import("./bootstrap");
